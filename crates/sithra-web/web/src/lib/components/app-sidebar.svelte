33a5f0c0be77436fd0dd8fc9f250a242723653ac
BERADQ <adqber123@outlook.com>
2025-07-23 19:51:06 +0800

    feat: constructing web page layout

---
+++ crates/sithra-web/web/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import HouseIcon from "@lucide/svelte/icons/house";
+    import Boxes from "@lucide/svelte/icons/boxes";
+    import Plus from "@lucide/svelte/icons/plus";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+    // Menu items.
+    const mainItems = [
+        {
+            title: "主页",
+            url: "#",
+            icon: HouseIcon,
+        },
+    ];
+
+    const pluginItems = [
+        {
+            title: "插件1",
+            url: "#",
+            icon: Boxes,
+        },
+        {
+            title: "插件2",
+            url: "#",
+            icon: Boxes,
+        },
+    ];
+</script>
+
+<Sidebar.Root variant="floating" class="pr-0">
+    <Sidebar.Content>
+        <Sidebar.Group>
+            <Sidebar.GroupLabel>Sithra</Sidebar.GroupLabel>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    {#each mainItems as item (item.title)}
+                        <Sidebar.MenuItem>
+                            <Sidebar.MenuButton>
+                                {#snippet child({ props })}
+                                    <a href={item.url} {...props}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </a>
+                                {/snippet}
+                            </Sidebar.MenuButton>
+                        </Sidebar.MenuItem>
+                    {/each}
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
+        <Sidebar.Group>
+            <Sidebar.GroupLabel>Plugins</Sidebar.GroupLabel>
+            <Sidebar.GroupAction title="添加插件">
+                <Plus /> <span class="sr-only">添加插件</span>
+            </Sidebar.GroupAction>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    {#each pluginItems as item (item.title)}
+                        <Sidebar.MenuItem>
+                            <Sidebar.MenuButton>
+                                {#snippet child({ props })}
+                                    <a href={item.url} {...props}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </a>
+                                {/snippet}
+                            </Sidebar.MenuButton>
+                        </Sidebar.MenuItem>
+                    {/each}
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
+    </Sidebar.Content>
+</Sidebar.Root>
--- crates/sithra-web/web/src/routes/+layout.svelte
+++ crates/sithra-web/web/src/routes/+layout.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+    import "../app.css";
+    import * as Sidebar from "$lib/components/ui/sidebar/index";
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+    import { Separator } from "$lib/components/ui/separator/index.js";
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import { ModeWatcher } from "mode-watcher";
+    import DarkToggle from "$lib/components/dark-toggle.svelte";
+
+    let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<Sidebar.Provider>
+    <AppSidebar />
+    <Sidebar.Inset>
+        <header
+            class="flex h-12 shrink-0 items-center justify-between px-2 pt-1"
+        >
+            <div class="flex items-center shrink-0 gap-2">
+                <Sidebar.Trigger />
+                <Separator
+                    orientation="vertical"
+                    class="mx-2 data-[orientation=vertical]:h-4"
+                />
+                <h1 class="font-bold">主页</h1>
+            </div>
+            <DarkToggle />
+        </header>
+        <div class="w-full h-full px-2 pb-2">
+            <main class="border rounded-md w-full h-full"></main>
+            {@render children?.()}
+        </div>
+    </Sidebar.Inset>
+</Sidebar.Provider>
